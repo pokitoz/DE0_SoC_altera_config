0ed86e421700790be71b705307c1ba8e52b3ba00
pokitoz <florian.depraz@epfl.ch>
2016-07-11 16:54:17 +0200

    Download more images by ssh

--- sw/get_images.sh
+++ sw/get_images.sh
@@ -26,6 +26,8 @@ do
 	sshpass -p "$env_sshpassword" scp $sshcommand:"*/*.tiff" "$result_folder_a"
 	sshpass -p "$env_sshpassword" scp $sshcommand:"*/*.ppm" "$result_folder_a"
 	sshpass -p "$env_sshpassword" scp $sshcommand:"*/*.bmp" "$result_folder_a"
+
+	sshpass -p "$env_sshpassword" scp $sshcommand:"*.bmp" "$result_folder_a"
 done
 
 
